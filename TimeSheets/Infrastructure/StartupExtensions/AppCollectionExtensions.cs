45248a7d82dc85d69f60ac00f30af31dd39c17d7
Батин Сергей <Hoshimin707@hotmail.com>
2022-02-16 17:21:55 +0300

    Добавьте файлы проекта.

---
+++ TimeSheets/Infrastructure/StartupExtensions/AppCollectionExtensions.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Builder;
+
+namespace TimeSheets.Infrastructure.StartupExtensions
+{
+    internal static class AppCollectionExtensions
+    {
+        public static void ConfigureSwagger(this IApplicationBuilder app)
+        {
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "API TimeSheets");
+            });
+        }
+    }
+}
---
+++ TimeSheets/Infrastructure/StartupExtensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.OpenApi.Models;
+
+namespace TimeSheets.Infrastructure.StartupExtensions
+{
+    internal static class ServiceCollectionExtensions
+    {
+        public static void ConfigureSwagger(this IServiceCollection services)
+        {
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo
+                {
+                    Title = "API TimeSheets",
+                    Version = "v1"
+                });
+            });
+        }
+    }
+}
