43db3e0248c2cdbfc6a071c1a408c801f77202eb
Батин Сергей <Hoshimin707@hotmail.com>
2022-02-18 16:05:35 +0300

    добавить решение урока 3

---
+++ RepositoriesLibrary/Repositories/Interfaces/IPersonRepository.cs
@@ -0,0 +1,16 @@
+﻿using ModelsLibrary.Models.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace RepositoriesLibrary.Repositories.Interfaces
+{
+    public interface IPersonRepository: IRepository<Person>
+    {
+        Task<Person> GetByNameAsync(string name, CancellationToken token);
+        Task<IEnumerable<Person>> GetPaginationAsync(int skip, int take, CancellationToken token);
+    }
+}
---
+++ RepositoriesLibrary/Repositories/Interfaces/IRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace RepositoriesLibrary.Repositories.Interfaces
+{
+    public interface IRepository<T> where T : class
+    {
+        Task CreateAsync(T item, CancellationToken token);
+        Task<T> GetByIdAsync(int id, CancellationToken token);
+        Task UpdateAsync(T item, CancellationToken token);
+        Task DeleteByIdAsync(int id, CancellationToken token);
+    }
+}
