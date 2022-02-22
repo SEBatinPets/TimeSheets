7072bb105cf2fba487c67909b7fd9b55eec2d036
Батин Сергей <Hoshimin707@hotmail.com>
2022-02-22 16:51:42 +0300

    исправить репозиторий

--- RepositoriesLibrary/Repositories/Interfaces/IPersonRepository.cs
+++ RepositoriesLibrary/Repositories/Interfaces/IPersonRepository.cs
@@ -8,9 +8,13 @@ using System.Threading.Tasks;
 
 namespace RepositoriesLibrary.Repositories.Interfaces
 {
-    public interface IPersonRepository: IRepository<Person>
+    public interface IPersonRepository
     {
         Task<IEnumerable<Person>> GetByNameAsync(string name, CancellationToken token);
         Task<IEnumerable<Person>> GetPaginationAsync(int skip, int take, CancellationToken token);
+        Task CreateAsync(Person item, CancellationToken token);
+        Task<Person> GetByIdAsync(int id, CancellationToken token);
+        Task<int> UpdateAsync(Person item, CancellationToken token);
+        Task DeleteByIdAsync(int id, CancellationToken token);
     }
 }
