d69250c0fcb57a5b41e360ac38c734dd01e0f05d
Батин Сергей <Hoshimin707@hotmail.com>
2022-03-03 17:12:01 +0300

    добавить тесты

---
+++ TimeSheetsTestsProject/UserController/DeleteTests.cs
@@ -0,0 +1,33 @@
+﻿using DomainLibrary.Domain.Managers.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+using ModelsLibrary.Models.DTO;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using TimeSheets.Controllers;
+using Xunit;
+
+namespace TimeSheetsTestsProject.UserController
+{
+    public class DeleteTests
+    {
+        [Fact]
+        public void Delete_return_Ok()
+        {
+            var tokent = new CancellationToken();
+            var userManager = new Mock<IUsersManager>();
+            userManager.Setup(
+                x => x.DeleteByIdAsync(It.IsAny<int>(), tokent)
+                ).Returns(Task.FromResult(200));
+
+            var userController = new UsersController(userManager.Object);
+            var result = userController.Delete(It.IsAny<int>(), tokent).Result;
+
+            _ = Assert.IsAssignableFrom<IActionResult>(result);
+        }
+    }
+}
---
+++ TimeSheetsTestsProject/UserController/GetTests.cs
@@ -0,0 +1,33 @@
+﻿using DomainLibrary.Domain.Managers.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+using ModelsLibrary.Models.DTO;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using TimeSheets.Controllers;
+using Xunit;
+
+namespace TimeSheetsTestsProject.UserController
+{
+    public class GetTests
+    {
+        [Fact]
+        public void Get_return_Ok()
+        {
+            var tokent = new CancellationToken();
+            var userManager = new Mock<IUsersManager>();
+            userManager.Setup(
+                x => x.GetByIdAsync(It.IsAny<int>(), tokent)
+                ).Returns(Task.FromResult<UserDto> (new UserDto()));
+
+            var userController = new UsersController(userManager.Object);
+            var result = userController.Get(It.IsAny<int>(), tokent).Result;
+
+            _ = Assert.IsAssignableFrom<IActionResult>(result);
+        }
+    }
+}
